be5058c3ecdb19fce9d32f5eee130732953aa85b
Byakoren <thomas.gouez-hoarau@live.fr>
2024-10-14 15:12:13 +0200

    edit

--- Python/Exo_cours_python.py
+++ Python/Exo_cours_python.py
@@ -177,36 +177,15 @@
 #     print("Vous êtes senior.")
 
 ######################################################################################
+n = 20
 
-heure_debut = int(input("Quel est l'heure de début de la location du velo (sous format 24h)? "))
-heure_fin = int(input("Quel est l'heure de fin de la location de vélo (sous format 24h)? "))
+sommePaire = 0
+sommeTotale = 0
+for i in range(1, n + 1) :
+    if (i % 2 == 0) :
+        sommePaire = sommePaire + i
+    
+    sommeTotale = sommeTotale + 1
 
-heure_1 = 0
-heure_2 = 0
-prix_location = 0
-
-if heure_debut < 0 or heure_debut > 24 or heure_fin < 0 or heure_fin > 24 :
-    print("L'heure de début et de fin doivent être comprise ans les 24heures.")
-if heure_debut == heure_fin :
-    print("Tu ne peux pas louer le vélo 1journée complète ou plus.")
-if heure_debut > heure_fin :
-    print("L'heure de début doit êter inférieur à celle de fin.")
-
-for heure in range(heure_debut, heure_fin):
-    if 0 <= heure < 7 or 17 <= heure < 24:
-        prix_location += 1  
-    else:
-        prix_location += 2  
-
-durer_location = heure_fin - heure_debut
-
-for heure in range(heure_debut,heure_fin):
-    if 0 <= heure < 7 or 17 <= heure < 24:
-        heure_1 += 1
-    else :
-        heure_2 += 1
-
-print("Vous avez loué votre vélo pendant: ", durer_location, "heures.")
-print(heure_1, "heure(s) au tarif horaire de 1.0 euro(s)")
-print(heure_2, "heure(s) au tarif horaire de 2.0 euro(s) ")
-print("Le prix de votre location sera de:", prix_location, "euro(s).")
+print(sommeTotale)
+print(sommePaire)
