dd4145dc40ce0cdb094b11f54e7f383acf84ccb3
Byakoren <thomas.gouez-hoarau@live.fr>
2024-11-22 10:57:30 +0100

    edit

--- Python/test.py
+++ Python/test.py
@@ -123,16 +123,80 @@
 # print("Résultat: ", resultat)                                #ça te donne la position de l'entier dans la liste.
 
 
-import random
+# import random
 
-def generate_phone_numbers(quantity):
-  phone_numbers = []
-  for _ in range(quantity):
-    number = "0" + str(random.choice([6, 7]))
-    for _ in range(8):
-      number += str(random.randint(0, 9))
-    phone_numbers.append(number)
-  return phone_numbers
+# def generate_phone_numbers(quantity):
+#   phone_numbers = []
+#   for _ in range(quantity):
+#     number = "0" + str(random.choice([6, 7]))
+#     for _ in range(8):
+#       number += str(random.randint(0, 9))
+#     phone_numbers.append(number)
+#   return phone_numbers
 
-numbers = generate_phone_numbers(30)
-print(numbers)
+# numbers = generate_phone_numbers(30)
+# print(numbers)
+
+
+# lloyd = {
+#   "name": "Lloyd",
+#   "homework": [90.0, 97.0, 75.0, 92.0],
+#   "quizzes": [88.0, 40.0, 94.0],
+#   "tests": [75.0, 90.0]
+# }
+# alice = {
+#   "name": "Alice",
+#   "homework": [100.0, 92.0, 98.0, 100.0],
+#   "quizzes": [82.0, 83.0, 91.0],
+#   "tests": [89.0, 97.0]
+# }
+# tyler = {
+#   "name": "Tyler",
+#   "homework": [0.0, 87.0, 75.0, 22.0],
+#   "quizzes": [0.0, 75.0, 78.0],
+#   "tests": [100.0, 100.0]
+# }
+
+# # Add your function below!
+# def average(numbers):
+#   total = sum(numbers)
+#   total = float(total)
+#   return total / len(numbers)
+
+# def get_average(student):
+#   homework = average(student["homework"])
+#   quizzes = average(student["quizzes"])
+#   tests = average(student["tests"])
+#   total = homework * 0.1 + quizzes * 0.3 + tests * 0.6 
+#   return total
+
+# def get_letter_grade(score):
+#   if score >= 90:
+#     return "A"
+#   elif score >= 80:
+#     return "B"
+#   elif score >= 70:
+#     return "C"
+#   elif score >= 60:
+#     return "D"
+#   else:
+#     return "F"
+
+
+# def get_class_average(class_list):
+#   results = []
+#   for student in class_list:
+#     results.append(get_average(student))
+#   return average(results)
+
+# print(get_class_average([lloyd, alice, tyler]))
+
+
+n = [3, 5, 7]
+
+def print_list(x):
+  for i in range(0, len(x)):
+    print(x[i])
+
+
+print_list(n)
